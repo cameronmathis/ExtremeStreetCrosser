5910fb0e47a1ed39041ce93ecc6478449ed48d07
cameronmathis <cmmathis98@gmail.com>
2021-03-11 14:26:26 -0600

    road moves

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 6f;
-
-    Rigidbody playerRigidbody;
+    public float xRange = 12.0f;
+    public float zTopRange = -4.5f;
+    public float zBottomRange = -12.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRigidbody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        move();
+        checkBounds();
+    }
+
+    void move()
     {
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         float moveVertical = Input.GetAxisRaw("Vertical");
@@ -24,5 +31,28 @@ public class PlayerController : MonoBehaviour
         transform.rotation = Quaternion.LookRotation(movement);
 
         transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+    }
+
+    void checkBounds()
+    {
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z > zTopRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zTopRange);
+        }
+
+        if (transform.position.z < zBottomRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBottomRange);
+        }
     }
 }
