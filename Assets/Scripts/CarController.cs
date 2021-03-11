87277172aeeaa4b2ce0791cc4e57588959bcec89
cameronmathis <cmmathis98@gmail.com>
2021-03-11 16:51:15 -0600

    cars and roads move now

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    public float movementSpeed = 6f;
+    public float xRange = 35.0f;
+    public float zTopRange = -4.5f;
+    public float zBottomRange = -26.9f;
+    public GameObject carPrefab;
+
+    private GameObject playerGameObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerGameObject = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        move();
+        checkBounds();
+    }
+    
+    void move()
+    {
+        float moveVertical = 0.0f;
+        if (playerGameObject.transform.position.z >= zTopRange)
+        {
+            moveVertical = Input.GetAxisRaw("Vertical");
+        }
+        Vector3 movement = new Vector3(3.0f * (transform.rotation.y / 0.7071068f), 0.0f, -moveVertical);
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+    }
 
+    void checkBounds()
+    {
+        if (transform.position.x < -xRange)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.z < zBottomRange)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/RoadController.cs
+++ Assets/Scripts/RoadController.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class RoadController : MonoBehaviour
 {
+    public float movementSpeed = 6f;
+    public float zTopRange = -4.5f;
+    public GameObject roadPrefab;
+
+    private Vector3 spawnPos = new Vector3(0.0f, 0.12f, 18.0f);
+    private GameObject playerGameObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerGameObject = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z < -15)
+        move();
+        checkBounds();
+    }
+
+    void move()
+    {
+        if (playerGameObject.transform.position.z >= zTopRange)
         {
-            Destroy(this);
+            float moveVertical = Input.GetAxisRaw("Vertical");
+            Vector3 movement = new Vector3(0.0f, 0.0f, -moveVertical);
+            transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+        }
+    }
+
+    void checkBounds()
+    {
+        if (transform.position.z < -26.9)
+        {
+            GameObject roadInstance = (GameObject)Instantiate(roadPrefab, spawnPos, roadPrefab.transform.rotation);
+            roadInstance.name = "Road";
+            Destroy(gameObject);
         }
     }
 }
